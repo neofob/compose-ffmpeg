35c422f65ae33b04f1ba4e5182206a12b2c3d382
Tuan T. Pham <tuan@vt.edu>
2021-03-08 19:38:22 -0500

    Update extract audio script
    Signed-off-by: Tuan T. Pham <tuan@vt.edu>

--- scripts/exau.sh
+++ scripts/exau.sh
@@ -7,7 +7,9 @@
 # Reference: https://gist.github.com/protrolium/e0dbd4bb0f1a396fcb55
 # ffmpeg -i test.mp4 -f mp3 -ab 320000 -vn test.mp3
 
-echo "Entering $0"
+# TODO; use map
+# https://stackoverflow.com/questions/21184846/ffmpeg-transcoding-multiple-input-files-to-multiple-output-files-with-single-co
+echo "> Entering $0"
 
 SRC_DIR=${SRC_DIR:=/tmp/workdir}
 FILE_PATTERN=${FILE_PATTERN='*.mp4 *.webm *.mkv'}
@@ -17,7 +19,8 @@ BIT_RATE=${BIT_RATE:=320000}
 # This works for Ubuntu/Debian where the 1st user is 1000:1000
 USER_ID=${USER_ID:=1000}
 GROUP_ID=${GROUP_ID:=1000}
-DRY_RUN=${DRY_RUN=0}
+DRY_RUN=${DRY_RUN:=0}
+CPUS=${CPUS:=`grep -c processor /proc/cpuinfo`}
 
 if [ ! -d $SRC_DIR ]; then
 	echo "$SRC_DIR does not exists!"
@@ -30,7 +33,8 @@ pushd $SRC_DIR >/dev/null
 
 for p in $FILE_PATTERN; do
 	for f in `ls $p`; do
-		out_file=$(echo $f | sed -e "s/\..*$/.$FORMAT/")
+        tmp=$(echo $f | sed -e "s/ /_/g" )
+		out_file=$(echo $tmp | sed -e "s/\..*$/.$FORMAT/")
 		# echo "$f $out_file"
 		ffmpeg -i $f -f $FORMAT -ab $BIT_RATE -vn $DEST_DIR/$out_file
 	done
@@ -40,4 +44,4 @@ chown -R $USER_ID:$GROUP_ID $DEST_DIR
 
 popd >/dev/null
 
-echo "Exiting $0"
+echo "< Exiting $0"
